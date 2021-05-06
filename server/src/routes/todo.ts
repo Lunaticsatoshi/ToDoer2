bb6e36ff188b29340b11b7eff60331d5c98ee825
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-05-06 17:25:47 +0530

    :package: Adding get and post routes in Todo routes

--- server/src/routes/todo.ts
+++ server/src/routes/todo.ts
@@ -1,6 +1,11 @@
 import {Router} from "express";
+import {getTodos,addTodos} from "../controller/todo";
 
 const router :Router = Router();
+
+router.get('/api/v1/todos', getTodos);
+
+router.post('/api/v1/addTodos', addTodos);
 
 
 export default router;
