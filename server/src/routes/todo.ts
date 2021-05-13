224d8614f933efbbf8ae32b07bf14a09673b5c82
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-05-13 17:42:10 +0530

    :zap: Update and Delete Todos Route

--- server/src/routes/todo.ts
+++ server/src/routes/todo.ts
@@ -1,11 +1,14 @@
 import {Router} from "express";
-import {getTodos,addTodos} from "../controller/todo";
+import {getTodos,addTodos,updateTodo,deleteTodo} from "../controller/todo";
 
 const router :Router = Router();
 
 router.get('/api/v1/todos', getTodos);
 
-router.post('/api/v1/addTodos', addTodos);
+router.post('/api/v1/addTodo', addTodos);
 
+router.put('/api/v1/updateTodo/:id',updateTodo);
+
+router.delete('/api/v1/deleteTodo/:id',deleteTodo);
 
 export default router;
