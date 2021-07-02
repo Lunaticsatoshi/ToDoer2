40fb1e10f9c279eedb611d80adec63a488f55b34
Lunaticsatoshi <deysarkersudeep@gmail.com>
2021-07-02 17:01:46 +0530

    :label: Adding types to Custom react hook

--- client/src/hooks/handleForm.ts
+++ client/src/hooks/handleForm.ts
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
 
-export const handleForm = (initialState = {}) => {
+type InitialState = {
+    title: string,
+    description: string,
+    status: boolean
+}
+
+export const handleForm = (initialState: InitialState) => {
   const [formData, setFormData] = useState(initialState);
 
   const handleInputChange = (e: React.FormEvent<HTMLInputElement>) => {
